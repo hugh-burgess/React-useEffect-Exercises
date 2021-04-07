96bfddde3006fd56f6e21833d0a567712c7c47e8
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-07 10:39:55 +0200

    touchups

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ function User({ title, first, last, photoUrl, gender }) {
   return (
     <div className="userBox">
       <div className={gender === "male" ? "maleBox" : "femaleBox"}>
-        <h2 className="titleName">
+        <h2 className="boxName">
           {title}. {first} {last}
         </h2>
         <img className="profilePic" src={photoUrl} alt={first} />
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -6,22 +6,16 @@ function UserList() {
   const [results, setResults] = useState(10);
   const [filter, setFilter] = useState("");
 
-const button = document.querySelector("button")
-
-
-
-
+  const button = document.querySelector("button");
 
   useEffect(() => {
-
-
     const baseUrl = "https://randomuser.me/api/?inc=email,gender,name,picture";
     let url = baseUrl;
     if (results) {
       url = `${baseUrl}&results=${results}`;
     } else {
       url = `${baseUrl}&results=10`;
-    } 
+    }
 
     fetch(url)
       .then((res) => res.json())
@@ -31,13 +25,20 @@ const button = document.querySelector("button")
       });
   }, [results]);
 
-
-
   function handleChangeEvent(event) {
     setResults(event.target.value);
   }
 
   function renderPosts() {
+    let filteredType;
+    if (filter === "female") {
+      filteredType = users.filter((user) => user.gender === "female");
+    } else if (filter === "male") {
+      filteredType = users.filter((user) => user.gender === "male");
+    } else {
+      filteredType = users;
+    }
+
     const userComponents = filteredType.map((user) => (
       <User
         key={user.email}
@@ -51,45 +52,43 @@ const button = document.querySelector("button")
     return userComponents;
   }
 
-  let filteredType;
-  if (filter === "female") {
-      filteredType = users.filter((user) => user.gender === "female")
-  } else if (filter === "male") {
-      filteredType = users.filter((user) => user.gender === "male")
-  } else {
-    filteredType = users;
-
+  function handleClickAll() {
+    setFilter("");
   }
-  
-
-function handleClickAll() {
-    setFilter("")
-}
-function handleClickMale() {
-    setFilter("male")
-}
-function handleClickFemale() {
-    setFilter("female")
-}
+  function handleClickMale() {
+    setFilter("male");
+  }
+  function handleClickFemale() {
+    setFilter("female");
+  }
 
   return (
     <div>
-    <h1 className="titleName">Random Friends Creator</h1>
+      <h1 className="titleName">Find-Me-A-Pal-Generator</h1>
       <p>Number of results: {users.length}</p>
-      
+
       <div className="wrapper">
-       <input
-        onChange={handleChangeEvent}
-        type="text"
-        placeholder="enter a number.."
-      />
+        <input
+          onChange={handleChangeEvent}
+          type="text"
+          placeholder="enter a number.."
+        />
       </div>
- 
-      
-      <button onClick={handleClickAll} className="headerButton" type="button" >All</button>
-      <button onClick={handleClickMale} className="headerButton" type="button" >Male</button>
-      <button onClick={handleClickFemale} className="headerButton" type="button" >Female</button>
-     
+
+      <button onClick={handleClickAll} className="headerButton" type="button">
+        All
+      </button>
+      <button onClick={handleClickMale} className="headerButton" type="button">
+        Male
+      </button>
+      <button
+        onClick={handleClickFemale}
+        className="headerButton"
+        type="button"
+      >
+        Female
+      </button>
+
       <div className="userSection">{renderPosts()}</div>
     </div>
   );
